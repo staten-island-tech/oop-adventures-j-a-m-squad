6b43840e27109111ad924c72610b04be51d72782
albert1berko <124071925+albert1berko@users.noreply.github.com>
2023-04-28 10:35:38 -0400

    e
    e

--- PokedexState.py
+++ PokedexState.py
@@ -2,7 +2,7 @@ import pygame
 from pygame.locals import *
 # pygame setup
 pygame.init()
-screen = pygame.display.set_mode((1080, 720), pygame.RESIZABLE)
+screen = pygame.display.set_mode((320, 224), pygame.RESIZABLE)
 clock = pygame.time.Clock()
 running = True
 dt = 0
@@ -13,7 +13,7 @@ pygame.mixer.music.load("assets\music\PokedexTheme.ogg")
 pygame.mixer.music.play(-1)
 
 scroll = pygame.mixer.Sound("assets\sounds\scrollMenu.ogg")
-class main_character
+
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -35,16 +35,22 @@ while running:
         pygame.mixer.Sound.play(scroll)
     if keys[pygame.K_a]:
         player_pos.x -= 300 * dt
+        pygame.mixer.Sound.play(scroll)
     if keys[pygame.K_d]:
         player_pos.x += 300 * dt
+        pygame.mixer.Sound.play(scroll)
     if keys[pygame.K_UP]:
         player_pos.y -= 300 * dt
+        pygame.mixer.Sound.play(scroll)
     if keys[pygame.K_DOWN]:
         player_pos.y += 300 * dt
+        pygame.mixer.Sound.play(scroll)
     if keys[pygame.K_LEFT]:
         player_pos.x -= 300 * dt
+        pygame.mixer.Sound.play(scroll)
     if keys[pygame.K_RIGHT]:
         player_pos.x += 300 * dt
+        pygame.mixer.Sound.play(scroll)
  
 
     # flip() the display to put your work on screen
