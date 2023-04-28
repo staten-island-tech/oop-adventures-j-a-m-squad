d378a5e56c294ddaef7a5dabb639f52846d2f4e7
SanchezJovanni <124072339+SanchezJovanni@users.noreply.github.com>
2023-04-28 19:57:48 -0400

    SEGASONIC CD

--- Stardust Speedway Zone.py
+++ Stardust Speedway Zone.py
@@ -13,13 +13,15 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 #Setup the players position on the screen
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2.1)
 #Load and play music
 pygame.mixer.music.load("assets\music\Stardust Speedway Bad Future.ogg")
 pygame.mixer.music.play(-1)
+#Load Sound Effects
+sonicJumpWacky = pygame.mixer.Sound("assets\sound\I'm outta here.ogg")
 #Load and scale Player
 player = pygame.image.load("assets\images\characters\egghead.jpeg")
-player = pygame.transform.scale(player, (300,400))
+player = pygame.transform.scale(player, (250,250))
 #Loads our Background and Foreground
 bg = pygame.image.load("assets\images\stages\Stardust Speedway\stardustBg.png").convert()
 fg = pygame.image.load("assets\images\stages\Stardust Speedway\stardustFloor.png").convert_alpha()
@@ -34,6 +36,11 @@ fg_rect = fg.get_rect()
 #Variabels for scrolling images
 scroll = 0
 tiles = math.ceil(SCREEN_WIDTH  / bg_width) + 2
+#Variables for jumping
+boingoing = False
+jumpgravity = 1
+jump = 20
+jumpVelocity = jump
 #Everything after this point is what happens while our game is running
 while running:
     #Quits the game
@@ -60,22 +67,34 @@ while running:
     keys = pygame.key.get_pressed()
     if keys[pygame.K_ESCAPE]:
         pygame.quit()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
     if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
+        player_pos.x -= 500 * dt
+        if keys[pygame.K_LSHIFT]:
+            player_pos.x -= 5000 * dt
     if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
-    if keys[pygame.K_UP]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_DOWN]:
-        player_pos.y += 300 * dt
+        player_pos.x += 500 * dt
+        if keys[pygame.K_LSHIFT]:
+            player_pos.x += 5000 * dt
+    if keys[pygame.K_SPACE]:
+        boingoing = True
+        if keys[pygame.K_LSHIFT]:
+           jump = 60
+           pygame.mixer.Sound.play(sonicJumpWacky)
     if keys[pygame.K_LEFT]:
         player_pos.x -= 300 * dt
+        if keys[pygame.K_LSHIFT]:
+            player_pos.x -= 5000 * dt
     if keys[pygame.K_RIGHT]:
         player_pos.x += 300 * dt
+        if keys[pygame.K_LSHIFT]:
+            player_pos.x += 5000 * dt
+    #Jump logic handled here
+    if boingoing:
+        player_pos.y -= jumpVelocity
+        jumpVelocity -= jumpgravity
+        if jumpVelocity < -jump:
+            boingoing = False
+            jumpVelocity =jump
     #Adds our work to the screen
     pygame.display.flip()
     #The FPS our game runs at
