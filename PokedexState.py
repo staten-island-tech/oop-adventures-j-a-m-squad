57827e1e96a0fe1d5ba52e15c98fb8f13abc87ee
SanchezJovanni <124072339+SanchezJovanni@users.noreply.github.com>
2023-04-28 17:23:54 -0400

    THE FULL ASS GAME

--- PokedexState.py
+++ PokedexState.py
@@ -1,64 +1,62 @@
+#Setup Python
 import pygame
 import math
 from pygame.locals import *
-# pygame setup
+#Start Pygame
 pygame.init()
-SCREEN_WIDTH = 1080
-SCREEN_HEIGHT = 720
-screen = pygame.display.set_mode((1080, 720))
-display = pygame.Surface((1080,720))
+#Screen Width and Height Variable (Makes it super easy if we ever want to change it)
+SCREEN_WIDTH = 1920
+SCREEN_HEIGHT = 1080
+#Setup Game
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 clock = pygame.time.Clock()
 running = True
 dt = 0
-
+#Setup the players position on the screen
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-
+#Load and play music
 pygame.mixer.music.load("assets\music\PokedexTheme.ogg")
 pygame.mixer.music.play(-1)
-
-size = (1080,720)
-berkovich = pygame.image.load("berkovich.jpeg")
+#Load and scale Player
+berkovich = pygame.image.load("assets\images\characters\berkovich.jpeg")
 berkovich = pygame.transform.scale(berkovich, (200,200))
-
-bg = pygame.image.load("stardustBg.png").convert()
-fg = pygame.image.load("stardustFloor.png").convert_alpha()
-bg = pygame.transform.scale(bg, size)
-fg = pygame.transform.scale(fg, size)
+#Loads our Background and Foreground
+bg = pygame.image.load("assets\images\stages\Stardust Speedway\stardustBg.png").convert()
+fg = pygame.image.load("assets\images\stages\Stardust Speedway\stardustFloor.png").convert_alpha()
+#Scales our background and foregroudn to the size of the screen
+bg = pygame.transform.scale(bg, (SCREEN_WIDTH, SCREEN_HEIGHT))
+fg = pygame.transform.scale(fg, (SCREEN_WIDTH, SCREEN_HEIGHT))
+#Sets the background and foreground as rectangles in order to manipulate them
 bg_width = bg.get_width()
 bg_rect = bg.get_rect()
 fg_width = fg.get_width()
 fg_rect = fg.get_rect()
-
+#Variabels for scrolling images
 scroll = 0
-tiles = math.ceil(SCREEN_WIDTH  / bg_width) + 1
-
+tiles = math.ceil(SCREEN_WIDTH  / bg_width) + 2
+#Everything after this point is what happens while our game is running
 while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
+    #Quits the game
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
+            #Stops running the game
             running = False
-    # fill the screen with a color to wipe away anything from last frame
-  #draw scrolling background
-    for i in range(0, tiles):
-        screen.blit(bg, (i * bg_width + scroll, 0))
-        bg_rect.x = i * bg_width + scroll
-        pygame.draw.rect(screen, (0, 0, 0), bg_rect, 1)
-
-  #scroll background
+    #Adds our background
+    for i in range(tiles):
+        #Forces our background to scroll
+        screen.blit(bg, (i * bg_width + scroll - bg_width, 0))
+    #How fast our Background and Foreground should scroll
+    #Keep the minus sign, Pygame hates if the background our Foreground goes backwards as it glitches them out
     scroll -= 25
-
-  #reset scroll
+    #Resets Scrolling
     if abs(scroll) > bg_width:
         scroll = 0
-
+    #Adds our Player
     screen.blit(berkovich, player_pos)
-    
+    #Add the foreground after the player for layering
     for i in range(0, tiles):
-        screen.blit(fg, (i * fg_width + scroll, 0))
-        fg_rect.x = i * fg_width + scroll
-        pygame.draw.rect(screen, (0, 0, 0), fg_rect, 1)
-
+        screen.blit(fg, (i * bg_width + scroll - bg_width, -100))
+    #All the keys our Game uses
     keys = pygame.key.get_pressed()
     if keys[pygame.K_ESCAPE]:
         pygame.quit()
@@ -78,14 +76,7 @@ while running:
         player_pos.x -= 300 * dt
     if keys[pygame.K_RIGHT]:
         player_pos.x += 300 * dt
- 
-
-    # flip() the display to put your work on screen
+    #Adds our work to the screen
     pygame.display.flip()
-
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
+    #The FPS our game runs at
     dt = clock.tick(60) / 1000
-
-pygame.quit()
