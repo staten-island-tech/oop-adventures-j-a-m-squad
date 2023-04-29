8c7985e2b9e9f458cfbc2650a56c3cf6d908fe97
SanchezJovanni <124072339+SanchezJovanni@users.noreply.github.com>
2023-04-28 23:31:12 -0400

    SONIC CD

--- Stardust Speedway Zone.py
+++ Stardust Speedway Zone.py
@@ -14,9 +14,9 @@ running = True
 dead = False
 dt = 0
 #Setup the players position on the screen
-player_pos = pygame.Vector2(screen.get_width() / 2.4, screen.get_height() / 2.1)
+player_pos = pygame.Vector2(screen.get_width() / 2.2, screen.get_height() / 2.1)
 #Load and play music
-pygame.mixer.music.load("assets\music\PokedexTheme.ogg")
+pygame.mixer.music.load("assets\music\Stardust Speedway Bad Future.ogg")
 pygame.mixer.music.play(-1)
 #Load Sound Effects
 sonicJump = pygame.mixer.Sound("assets\sounds\sonicJump.ogg")
