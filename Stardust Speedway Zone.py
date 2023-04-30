5c6a32a9459559357504acac4eaebcd5819291ff
SanchezJovanni <124072339+SanchezJovanni@users.noreply.github.com>
2023-04-29 21:08:59 -0400

    SEGASONIC CD 2011

--- Stardust Speedway Zone.py
+++ Stardust Speedway Zone.py
@@ -1,6 +1,7 @@
 #Setup Python
 import pygame
 import math
+import time
 from pygame.locals import *
 #Start Pygame
 pygame.init()
@@ -13,16 +14,20 @@ clock = pygame.time.Clock()
 running = True
 dead = False
 dt = 0
-#Setup the players position on the screen
-player_pos = pygame.Vector2(screen.get_width() / 1.75, screen.get_height() / 2.1)
+timer = 21
+#Setup the players and enemys position on the screen
+player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2.1)
+player2_pos = pygame.Vector2(screen.get_width()/ 4, screen.get_height() / 2.1)
 #Load and play music
-pygame.mixer.music.load("assets\music\Stardust Speedway.ogg")
+pygame.mixer.music.load("assets\music\Stardust Speedway Bad Future.ogg")
 pygame.mixer.music.play(-1)
 #Load Sound Effects
 sonicJump = pygame.mixer.Sound("assets\sounds\sonicJump.ogg")
 sonicJumpWacky = pygame.mixer.Sound("assets\sounds\I'm outta here.ogg")
-#Loads the Player
-player = pygame.transform.scale(pygame.image.load("assets\images\characters\egghead.jpeg"), (250,250))
+#Loads the Player and Enemy
+prey = pygame.transform.scale(pygame.image.load("assets\images\characters\Berkovich.jpeg"), (250,250))
+bystander = pygame.transform.scale(pygame.image.load("assets\images\characters\When you outside and smell that ZAZA.png"), (250,250))
+predator = pygame.transform.scale(pygame.image.load("assets\images\characters\egghead.jpeg"), (250,250))
 #Loads our Background and Foreground and scales them to the size of our screen
 bg = pygame.transform.scale(pygame.image.load("assets\images\stages\Stardust Speedway\stardustBg.png").convert(), (SCREEN_WIDTH, SCREEN_HEIGHT))
 fg = pygame.transform.scale(pygame.image.load("assets\images\stages\Stardust Speedway\stardustFloor.png").convert_alpha(), (SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -34,6 +39,8 @@ fg_rect = fg.get_rect()
 #Variabels for scrolling images
 scroll = 0
 tiles = math.ceil(SCREEN_WIDTH  / bg_width) + 2
+#Variables for enemy
+hunting = False
 #Variables for jumping
 boingoing = False
 jumpGravity = 1
@@ -52,20 +59,20 @@ while running:
                 pygame.mixer.Sound.play(sonicJump)
             if keys[pygame.K_LSHIFT] and keys[pygame.K_SPACE]:
                 pygame.mixer.Sound.play(sonicJumpWacky)
-
     #Adds our background
     for i in range(tiles):
         #Forces our background to scroll
         screen.blit(bg, (i * bg_width + scroll - bg_width, 0))
     #How fast our Background and Foreground should scroll
     #Change the minus sign to the plus sign to make everything go backwards or vice versa
-    scroll -= 100
+    scroll -= 88
     #Resets Scrolling
     if abs(scroll) > bg_width:
         scroll = 0
-    #Adds our Player
-    screen.blit(player, player_pos)
-    #Add the foreground after the player for layering
+    #Adds our Player, Player 2, and enemy
+    screen.blit(prey, player_pos)  
+    screen.blit(bystander, player2_pos)
+    #Add the foreground after the player and enemy for layering
     for i in range(0, tiles):
         screen.blit(fg, (i * bg_width + scroll - bg_width, -100))
     #All the keys our Game uses
@@ -74,11 +81,13 @@ while running:
         pygame.quit()
     #Every key below this is keys for movement
     if keys[pygame.K_a]:
-        player_pos.x -= 500 * dt
+        player_pos.x -= 1000 * dt
+        player2_pos.x -= 500 * dt
         if keys[pygame.K_LSHIFT]:
             player_pos.x -= 5000 * dt
     if keys[pygame.K_d]:
-        player_pos.x += 500 * dt
+        player_pos.x += 1000 * dt
+        player2_pos.x += 500 * dt
         if keys[pygame.K_LSHIFT]:
             player_pos.x += 5000 * dt
     #Enables Jumping        
@@ -88,16 +97,19 @@ while running:
            jump = 60
     #Alternative keys for movement
     if keys[pygame.K_LEFT]:
-        player_pos.x -= 500 * dt
+        player_pos.x -= 1000 * dt
+        player2_pos.x -= 500 * dt
         if keys[pygame.K_LSHIFT]:
             player_pos.x -= 5000 * dt
     if keys[pygame.K_RIGHT]:
-        player_pos.x += 500 * dt
+        player_pos.x += 1000 * dt
+        player2_pos.x += 500 * dt
         if keys[pygame.K_LSHIFT]:
             player_pos.x += 5000 * dt
     #Jump logic handled here
     if boingoing:
         player_pos.y -= jumpVelocity
+        player2_pos.y -= jumpVelocity
         jumpVelocity -= jumpGravity
         if jumpVelocity < -jump:
             boingoing = False
