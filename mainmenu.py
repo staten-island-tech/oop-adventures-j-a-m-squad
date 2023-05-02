b637ab8a64d4d4014129df70828c88da2839e97b
michaelyan69 <124071946+michaelyan69@users.noreply.github.com>
2023-05-02 10:56:58 -0400

    commit is comprende

--- mainmenu.py
+++ mainmenu.py
@@ -3,10 +3,10 @@ from button import Button
 import math
 pygame.init()
 
-SCREEN = pygame.display.set_mode((1280, 720))
+SCREEN = pygame.display.set_mode((1920, 1080))
 pygame.display.set_caption("Menu")
 
-BG = pygame.image.load("tails.png")
+BG = pygame.transform.scale(pygame.image.load("tails.png").convert(), (1920, 1080))
 
 def get_font(size): # Returns Press-Start-2P in the desired size
     return pygame.font.Font("font.ttf", size)
@@ -15,8 +15,8 @@ def play():
     while True:
         PLAY_MOUSE_POS = pygame.mouse.get_pos()
 
-        SCREEN_WIDTH = 1280
-        SCREEN_HEIGHT = 720
+        SCREEN_WIDTH = 1920
+        SCREEN_HEIGHT = 1080
         #Setup Game
         screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.RESIZABLE)
         clock = pygame.time.Clock()
@@ -220,13 +220,13 @@ def main_menu():
         MENU_MOUSE_POS = pygame.mouse.get_pos()
 
         MENU_TEXT = get_font(100).render("MAIN MENU", True, "#b68f40")
-        MENU_RECT = MENU_TEXT.get_rect(center=(640, 100))
+        MENU_RECT = MENU_TEXT.get_rect(center=(1000, 300))
 
-        PLAY_BUTTON = Button(image=pygame.image.load("lol.png"), pos=(640, 250), 
+        PLAY_BUTTON = Button(image=pygame.image.load("lol.png"), pos=(1000, 450), 
                             text_input="PLAY", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-        OPTIONS_BUTTON = Button(image=pygame.image.load("lol.png"), pos=(640, 400), 
+        OPTIONS_BUTTON = Button(image=pygame.image.load("lol.png"), pos=(1000, 600), 
                             text_input="OPTIONS", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
-        QUIT_BUTTON = Button(image=pygame.image.load("lol.png"), pos=(640, 550), 
+        QUIT_BUTTON = Button(image=pygame.image.load("lol.png"), pos=(1000, 750), 
                             text_input="QUIT", font=get_font(75), base_color="#d7fcd4", hovering_color="White")
 
         SCREEN.blit(MENU_TEXT, MENU_RECT)
