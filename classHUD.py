13b654b4c82da52e788bf5a6f1cc480a91b21d7a
SanchezJovanni <124072339+SanchezJovanni@users.noreply.github.com>
2023-05-15 10:32:03 -0400

    daFont

--- classHUD.py
+++ classHUD.py
@@ -5,5 +5,7 @@ pygame.font.init()
 
 class HUD:
     def __init__(self):
+        self.daFont = pygame.font.Font("PhantomMuff Full Letters 1.1.5.ttf", 64)
         self.message = "This is my HUD. I love it <3"
+        self.string = 
 
