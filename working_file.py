f9624884c2a131c6a0bf4181ada5bbb18aa36548
SanchezJovanni <124072339+SanchezJovanni@users.noreply.github.com>
2023-05-14 20:36:28 -0400

    Revert "e"
    This reverts commit 2f2c76718df31ab886f54a64f3d1b27a3269851c.

--- working_file.py
+++ working_file.py
@@ -2,7 +2,8 @@ import pygame, sys
 from button import Button
 import math
 import menu
-import random
+import os
+from pyvidplayer import Video
 pygame.init()
 
 SCREEN = pygame.display.set_mode((1920, 1080))
@@ -33,44 +34,155 @@ def play():
         start = pygame.time.get_ticks()
         running = True
         dt = 0
+        saving = 0
         FontSonic = pygame.font.Font("assets/fonts/sonic1.ttf", 64)
         #Setup the players and enemys position on the screen
         player_pos = pygame.Vector2(screen.get_width() / 1.5, screen.get_height() / 1.92)
         player2_pos = pygame.Vector2(screen.get_width()/ 1.75, screen.get_height() / 1.75)
         enemy_pos = pygame.Vector2(-555, 360)
         SaveYour_pos = pygame.Vector2(1700, 400)
-        commit_pos = pygame.Vector2(1700,400)
+        commit_pos = pygame.Vector2(1700,600)
         #Load and play music
-        pygame.mixer.music.load("assets\music\Trip to Burger King.ogg")
-        pygame.mixer.music.play(-1)
+        # pygame.mixer.music.load("assets\sounds\Save Your Work audio.mp3")
+        # pygame.mixer.music.play(-1)
+        #Load Sound Effects
+        sonicJump = pygame.mixer.Sound("assets\sounds\sonicJump.ogg")
+        sonicJumpWacky = pygame.mixer.Sound("assets\sounds\I'm outta here.ogg")
+        SaveYourAudio = pygame.mixer.Sound("assets\sounds\Save Your Work audio.mp3")
+        #Loads the Player and Enemy
+        prey = pygame.transform.scale(pygame.image.load("assets\images\characters\Berkovich.jpeg"), (200,200))
+        bystander = pygame.transform.scale(pygame.image.load("assets\images\characters\When you outside and smell that ZAZA.png"), (150,150))
+        predator = pygame.transform.scale(pygame.image.load("Untitled.png"), (300,280))
+        #Loads our Background and Foreground and scales them to the size of our screen
+        bg = pygame.transform.scale(pygame.image.load("assets\images\stages\Stardust Speedway\stardustBg.png").convert(), (SCREEN_WIDTH, SCREEN_HEIGHT))
+        fg = pygame.transform.scale(pygame.image.load("assets\images\stages\Stardust Speedway\stardustFloor.png").convert_alpha(), (SCREEN_WIDTH+500, SCREEN_HEIGHT+500))
+        #Sets the background and foreground as rectangles in order to manipulate them later
+        bg_width = bg.get_width()
+        bg_rect = bg.get_rect()
+        fg_width = fg.get_width()
+        fg_rect = fg.get_rect()
+        #Variabels for scrolling images
+        bgSpeed = 0
+        floorSpeed = 0
+        tiles = math.ceil(SCREEN_WIDTH  / bg_width) + 2
+        #Variables for jumping
+        boingoing = False
+        jumpGravity = 1
+        jump = 20
+        jumpVelocity = jump
+        #Variables for HUD
+        scoreLabel = FontSonic.render('SCORE', False, (255,255,0))
+        scoreValueText = FontSonic.render('0', False, (255,255,255))
+        timeLabel = FontSonic.render('TIME', False, (255,255,0))
+        ringsLabel = FontSonic.render('RINGS', False, (255,255,0))
+        ringsValueText = FontSonic.render('0', False, (255,255,255))
         healthSprite = pygame.transform.scale(pygame.image.load("assets/images/UI/sonicLifeCounter.png"), (75,50))
         healthValueText = FontSonic.render('3', False, (255,255,255))
         SaveYour = FontSonic.render('SAVE YOUR WORK', False, (255,255,0))
-                commit =  FontSonic.render('COMMIT TO GITHUB', False, (255,255,0))
+        commit =  FontSonic.render('COMMIT TO GITHUB', False, (255,255,0))
         #Variables for Time
         sonicCDMil = 0
         sonicCDSec = 0
+        sonicCDMin = 0
+        finalMil = 00
+        finalSec = 00
+        #Everything after this point is what happens while our game is running
+        while running:
+            SaveYour_pos = pygame.Vector2(1500, 400)
+            commit_pos = pygame.Vector2(1500,600)
+            saving += 1
+            #Quits the game
+            for event in pygame.event.get():
+                saving += 1
+                if event.type == pygame.QUIT:
+                    #Stops running the game
+                    running = False
+                #The reason the sound effects are put here is to only play them after the spacebar is released
+                elif event.type == pygame.KEYUP:
+                    if keys[pygame.K_SPACE]:
+                        pygame.mixer.Sound.play(sonicJump)
+                    if keys[pygame.K_LSHIFT] and keys[pygame.K_SPACE]:
+                        pygame.mixer.Sound.play(sonicJumpWacky)
+            #Time logic handled here
+            counting = pygame.time.get_ticks() - start
+            if (math.floor(start) >= 0):
+                sonicCDSec = (math.floor((math.floor(counting) / 1000) % 60))
+                sonicCDMil = ((round(((math.floor(counting)) % 1000) / 10) % 100))
+                sonicCDMin = (math.floor(math.floor((math.floor(counting) / 1000) / 60) % 60))
 
+            if (sonicCDMil < 10):
+                finalMil = sonicCDMil
+            else:
+                finalMil = sonicCDMil
+            if (sonicCDSec < 10):
+                finalSec = sonicCDSec
+            else:
+                finalSec = sonicCDSec
+
+            timeValue = "%s'0%s'%s" % (sonicCDMin, sonicCDSec, sonicCDMil)
+            if (sonicCDSec >= 10):
+                timeValue = "%s'%s'%s" % (sonicCDMin, sonicCDSec, sonicCDMil)
+
+            timeValueText = FontSonic.render(timeValue, False, (255,255,255))
+            #Adds our background
+            for i in range(tiles):
+                #Forces our background to scroll
+                screen.blit(bg, (i * bg_width + bgSpeed - bg_width, 0))
+            #How fast our Background and Foreground should scroll
+            #Change the minus sign to the plus sign to make everything go backwards or vice versa
+            bgSpeed -= 25
+            floorSpeed += 25
+            #Resets Scrolling
+            if abs(bgSpeed) > bg_width:
+                bgSpeed = 0
+            if abs(floorSpeed) > bg_width:
+                floorSpeed = 0
+            #Adds our Player, Player 2, and enemy
             screen.blit(predator, enemy_pos)
             screen.blit(bystander, player2_pos) 
-            screen.blit(prey, player_pos) 
-            screen.blit(SaveYour, (1700,400))
-            if sonicCDSec == 10:
-                
-                screen.blit(SaveYour, SaveYour_pos)
-                if SaveYour_pos.x != 0:
-                    SaveYour_pos.x += -35
-            if sonicCDSec == 12:
-                screen.blit(commit, commit_pos) 
-                if commit_pos.x != 0:
-                    commit_pos.x += -35
+            screen.blit(prey, player_pos)    
+                   
+            """ def attack(sec):
+                if sonicCDSec == sec:
+                    screen.blit(SaveYour, SaveYour_pos)
+                    if SaveYour_pos.x != 0:
+                        SaveYour_pos.x += -30
+                        pygame.mixer.Sound.play(SaveYourAudio)
+                if sonicCDSec == sec + 1:
+                    screen.blit(commit, commit_pos)
+                    if commit_pos.x != 0:
+                        commit_pos.x += -30 * dt
+            attack(1)
+            def Enemy_attack(z):
+                SaveYour_pos = pygame.Vector2(1600, 400)
+                commit_pos = pygame.Vector2(1600,600)
+                if sonicCDSec == z:
+                    screen.blit(SaveYour, SaveYour_pos)
+                    SaveYour_pos.x -= 35
+                    pygame.mixer.Sound.play(SaveYourAudio)
+                if sonicCDSec == z + 1:
+                    screen.blit(commit, commit_pos) 
+                    commit_pos.x -= 35       """
+            
             #Get the enemy into frame
             if sonicCDSec == 8:
                 if enemy_pos.x != 555:
                     enemy_pos.x += 35
-            if sonicCDMin == 2 and sonicCDSec == 10:
-                if enemy_pos.x != -55:
-                    enemy_pos.x -= 55
+            if sonicCDSec == 12:
+                if enemy_pos.x != 300:
+                    enemy_pos.x += -35
+            if sonicCDSec == 14:
+                if enemy_pos.x != 555:
+                    enemy_pos.x +=35
+            if sonicCDSec == 10:
+                screen.blit(SaveYour, SaveYour_pos)
+                if SaveYour_pos.x != 0:
+                    SaveYour_pos.x += -35
+                    pygame.mixer.Sound.play(SaveYourAudio)
+            if sonicCDSec == 12:
+                screen.blit(commit, commit_pos)
+                if commit_pos.x != 0:
+                    commit_pos.x += -35
             if player_pos.x > 1750 :
                 player_pos.x = 1700
                 player2_pos.x = 1700
@@ -84,7 +196,13 @@ def play():
                 player_pos.y = 1
                 player2_pos.y = 1
             
-            #Add the foreground after the player and enemy for layering
+            #Add the foreground after the player and enemy for layering            
+            """ for i in range(0, tiles):
+                screen.blit(SaveYour, (i * bg_width + floorSpeed - bg_width, +400))
+                pygame.mixer.Sound.play(SaveYourAudio)
+            for i in range(0, tiles):
+                screen.blit(commit, (i * bg_width + bgSpeed - bg_width, +650))
+                pygame.mixer.Sound.play(SaveYourAudio) """
             for i in range(0, tiles):
                 screen.blit(fg, (i * bg_width + floorSpeed - bg_width, -500))
             #Add the HUD above everything else
@@ -94,7 +212,7 @@ def play():
             screen.blit(timeValueText, (175,75))
             screen.blit(ringsLabel, (25, 135))
             screen.blit(ringsValueText, (374, 135))
-            screen.blit(healthSprite, (30, 888))
+            screen.blit(healthSprite, (30, 900))
             screen.blit(healthValueText, (118, 892))
             #All the keys our Game uses
             keys = pygame.key.get_pressed()
