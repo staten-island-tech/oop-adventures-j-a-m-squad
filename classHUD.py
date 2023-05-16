2553b9fabf9d71c8452921e4640b4eb1729c7d73
SanchezJovanni <124072339+SanchezJovanni@users.noreply.github.com>
2023-05-16 10:43:06 -0400

    wow

--- classHUD.py
+++ classHUD.py
@@ -5,7 +5,7 @@ pygame.font.init()
 
 class HUD:
     def __init__(self):
-        self.daFont = pygame.font.Font("soniccd.ttf", 25)
+        self.daFont = pygame.font.Font("sonic1.ttf", 44)
 
         self.scoreLabel = self.daFont.render("SCORE", False, (255,255,0))
         self.scoreLabelShadow = self.daFont.render("SCORE", False, (0,0,0))
@@ -15,10 +15,13 @@ class HUD:
         self.ringsLabelShadow = self.daFont.render("RINGS", False, (0,0,0))
 
         self.display = pygame.display.get_surface()
-        self.display.blit(self.scoreLabelShadow, (12,10))
-        self.display.blit(self.scoreLabel, (10,10))
-        self.display.blit(self.timeLabelShadow, (12,40))
-        self.display.blit(self.timeLabel, (10,40))
+        self.display.blit(self.scoreLabelShadow, (12,5))
+        self.display.blit(self.scoreLabel, (10,5))
+        self.display.blit(self.timeLabelShadow, (12,45))
+        self.display.blit(self.timeLabel, (10,45))
+        self.display.blit(self.ringsLabelShadow, (12, 80))
+        self.display.blit(self.ringsLabel, (10,80))
+
         
 
 
