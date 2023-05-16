4d2ff78550c3a6c932e9220d06fcbe9ab4997aef
SanchezJovanni <124072339+SanchezJovanni@users.noreply.github.com>
2023-05-16 10:23:59 -0400

    wow

--- classHUD.py
+++ classHUD.py
@@ -6,10 +6,11 @@ pygame.font.init()
 class HUD:
     def __init__(self, urFont):
         self.daFont = pygame.font.Font(urFont, 25)
-        self.message = "SCORE"
-        self.string = self.daFont.render(self.message, False, (255,255,0))
-        self.stringShadow = self.daFont.render(self.message, False, (0,0,0))
+        self.scoreText = self.daFont.render("SCORE", False, (255,255,0))
+        self.stringShadow = self.daFont.render("SCORE", False, (0,0,0))
         self.display = pygame.display.get_surface()
-        self.display.blit(self.string, (10,10))
+        self.display.blit(self.stringShadow, (12,10))
+        self.display.blit(self.scoreText, (10,10))
+        
 
 
