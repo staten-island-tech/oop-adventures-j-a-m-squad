b13015cdd52950299c1caea63e0aa311547ac02f
SanchezJovanni <124072339+SanchezJovanni@users.noreply.github.com>
2023-05-15 21:38:37 -0400

    wow

--- classHUD.py
+++ classHUD.py
@@ -8,6 +8,7 @@ class HUD:
         self.daFont = pygame.font.Font(urFont, 25)
         self.message = "SCORE"
         self.string = self.daFont.render(self.message, False, (255,255,0))
+        self.stringShadow = self.daFont.render(self.message, False, (0,0,0))
         self.display = pygame.display.get_surface()
         self.display.blit(self.string, (10,10))
 
--- main.py
+++ main.py
@@ -19,7 +19,7 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill("blue")
 
     pygame.draw.circle(screen, "red", player_pos, 40)
 
