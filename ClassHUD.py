3b1b67f244f9e8569667f883a7cfa0c234122907
SanchezJovanni <124072339+SanchezJovanni@users.noreply.github.com>
2023-06-03 15:11:44 -0400

    oo

--- ClassHUD.py
+++ ClassHUD.py
@@ -67,7 +67,7 @@ class HUD:
         self.screen.blit(self.healthSprite, (30, 1000))
         self.screen.blit(self.healthValueText, (118, 992))
 
-        if sonicCDSec == 30:
+        if sonicCDMin == 4 and sonicCDSec == 11:
             pygame.quit()
             os.system('python VictorySubstate.py')
 
