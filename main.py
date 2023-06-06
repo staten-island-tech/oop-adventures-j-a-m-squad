a0d7285fcd3fdb537feaf70c45b2f7853b371f63
SanchezJovanni <124072339+SanchezJovanni@users.noreply.github.com>
2023-06-06 10:22:00 -0400

    wow

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ def play():
                 screen.blit(fg, (i * bg_width + floorSpeed - bg_width, 0))
             #Add the HUD above everything else
             HUD(start)
-            HUD.updateScore(start)
+            HUD.updateScore(1)
             # #All the keys our Game uses
             keys = pygame.key.get_pressed()
             if keys[pygame.K_ESCAPE]:
